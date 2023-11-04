1a66123306d47580cd529cefed5c7dc6e689ea57
Viktor <viktor.riabyy@gmail.com>
2023-11-04 21:26:40 +0200

    add phone navigation
    first upload

--- script.js
+++ script.js
@@ -1,14 +1,69 @@
 import { Grid } from "./grid.js";
 import { Tile } from "./tile.js";
+import { Device } from "./device.js";
 
 const gameBord = document.getElementById("game-board");
-
 const endScreen = document.querySelector(".banner");
 
+const device = new Device();
 const grid = new Grid(gameBord);
 grid.getRandomEmptyCell().linkTile(new Tile(gameBord));
 grid.getRandomEmptyCell().linkTile(new Tile(gameBord));
-setupInputOnce();
+
+if (device.isMobile.any()) {
+  handleTouchMove();
+} else {
+  setupInputOnce();
+}
+
+let touchStartX = 0;
+let touchStartY = 0;
+
+gameBord.addEventListener("touchstart", handleTouchStart, false);
+gameBord.addEventListener("touchmove", handleTouchMove, false);
+
+function handleTouchStart(event) {
+  touchStartX = event.touches[0].clientX;
+  touchStartY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+  if (event.touches.length > 0) {
+    const touchEndX = event.touches[0].clientX;
+    const touchEndY = event.touches[0].clientY;
+
+    const deltaX = touchEndX - touchStartX;
+    const deltaY = touchEndY - touchStartY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      if (deltaX > 0) {
+        // Swipe right
+        if (canMoveRight()) {
+          moveRight();
+        }
+      } else {
+        // Swipe left
+        if (canMoveLeft()) {
+          moveLeft();
+        }
+      }
+    } else {
+      if (deltaY > 0) {
+        // Swipe down
+        if (canMoveDown()) {
+          moveDown();
+        }
+      } else {
+        // Swipe up
+        if (canMoveUp()) {
+          moveUp();
+        }
+      }
+    }
+    touchStartX = 0;
+    touchStartY = 0;
+  }
+}
 
 function setupInputOnce() {
   window.addEventListener("keydown", handleInput, { once: true });
